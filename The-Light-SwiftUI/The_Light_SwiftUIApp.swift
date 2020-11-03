7a84609fec1e21372cc3e2277402c5c6d62d435b
SorokinN <nikolay@sorokin.dev>
2020-11-03 03:09:44 +0300

    Initial Commit

---
+++ The-Light-SwiftUI/The_Light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Light_SwiftUIApp.swift
+//  The-Light-SwiftUI
+//
+//  Created by Nikolay Sorokin on 03.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct The_Light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
